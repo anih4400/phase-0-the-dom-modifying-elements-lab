5b28c425c8e4385989671a764c1b7c8666657f6a
AnishPatel <anish4400@gmail.com>
2022-12-06 09:12:48 -0500

    commit tree 141b44d11e69cf7d89537dae9ad10111a9d015b0

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+// Write your code here
+main.remove();
+
+const newHeader = document.createElement("h1");
+const newText = document.createTextNode("Anish is the champion")
+
+newHeader.setAttribute("id", "victory");
+newHeader.appendChild(newText);
